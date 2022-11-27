0e549e61c7069c6cfec002cb58dcbc2067ba3098
KnIfER <302772670@qq.com>
2022-11-27 15:56:54 +0800

    use asset file

--- tesseract4android/src/main/java/com/googlecode/tesseraction/PluginFileProvider.java
+++ tesseract4android/src/main/java/com/googlecode/tesseraction/PluginFileProvider.java
@@ -4,12 +4,16 @@ import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.ParcelFileDescriptor;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class PluginFileProvider extends ContentProvider {
 	public final static String baseUri = "content://com.googlecode.tesseraction/";
@@ -59,7 +63,24 @@ public class PluginFileProvider extends ContentProvider {
 	@Override
 	public AssetFileDescriptor openAssetFile(Uri uri, String mode) throws FileNotFoundException {
 		CMN.Log("PluginFileProvider::openAssetFile", uri, mode, uri.getPath());
-		
+		if (uri.getPath().equals("/tessdata/chi_sim.traineddata")
+			|| uri.getPath().equals("/tessdata/eng.traineddata")) {
+			File file = new File(getContext().getExternalFilesDir(null), uri.getPath());
+			if (!file.exists()) {
+				try {
+					InputStream input = getContext().getAssets().open("fast" + uri.getPath());
+					file.getParentFile().mkdirs();
+					FileOutputStream fout = new FileOutputStream(file);
+					byte[] data = new byte[4096];
+					int len;
+					while((len=input.read(data))!=-1)
+						fout.write(data, 0, len);
+					fout.close();
+				} catch (Exception e) {
+					CMN.Log(e);
+				}
+			}
+		}
 		return super.openAssetFile(uri, mode);
 	}
 	
